f3f31c24b0a093b822a359dc45e9c2f069263973
Suyash Labde <suyashlabde123@gmail.com>
2024-06-17 18:33:40 +0530

    redux and api setup done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import {React,useEffect} from 'react'
 import Navbar from '../components/Navbar'
+import { useAppDispatch, useAppSelector } from '../hooks/useApp'
+import { getHomePageVideos } from '../store/reducers/getHomePageVideos'
 
 function Home() {
+  const dispatch = useAppDispatch()
+  const videos = useAppSelector(state => state.youtubeApp.videos)
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+  }, [dispatch])
   return (
-    <Navbar/>
+    <Navbar />
   )
 }
 
